5f459f31051f3208cf6812b42380320fdab746c0
watchout254 <mukenyadaniel299@gmail.com>
2024-03-17 11:32:20 +0300

    Hackathon my guy

--- functions.dart
+++ functions.dart
@@ -1,2 +1,30 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+
+import 'dart:io';
+void add(int num1, int num2){
+  int sum = num1 + num2;
+  print("The sum is: $sum");
+}
+
+void mult(int num1, int num2){
+  int product = num1 * num2;
+  print("The product is: $product");
+}
+
+
+void main() {
+  print("😎WELCOME TO MUKENYAS SCHOOL😎");
+  stdout.write("Enter digit one: ");
+  String dig1 = stdin.readLineSync()!;
+  int num1 = int.tryParse(dig1)??0;
+
+  stdout.write("Enter digit two: ");
+  String dig2 = stdin.readLineSync()!;
+  int num2 = int.tryParse(dig2)??0;
+
+  add(num1,num2);
+  mult(num1,num2);
+
+  print("Have a splendid day.");
+}
+
+
--- program1..dart
+++ program1..dart
@@ -1,2 +1,23 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+import 'dart:io';
+
+void main() {
+  print("😎WELCOME TO MUKENYAS SCHOOL😎");
+  stdout.write("Enter your name: ");
+  String name = stdin.readLineSync()!;
+
+  stdout.write("Enter your age: ");
+  String age = stdin.readLineSync()!;
+  int miaka = int.tryParse(age) ?? 0;
+
+  stdout.write("Enter your school: ");
+  String school = stdin.readLineSync()!;
+
+  stdout.write("Enter your hobby: ");
+  String hobby = stdin.readLineSync()!;
+
+  // Print the information in one sentence
+  print(
+      "My name is $name. I am $miaka years old, studying at $school, and my hobby is $hobby.");
+  print("Have a good day $name 🤗");
+}
